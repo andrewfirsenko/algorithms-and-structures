105db9b0f9676c1f0bc3e810f692484cd17f6dd6
Andrew Firsenko <andrewfirsenko@gmail.com>
2025-01-29 17:47:44 +0300

    Add xcworkproject

---
+++ Playground/Karatsuba/karatsuba.cpp
@@ -0,0 +1,8 @@
+//
+//  karatsuba.cpp
+//  Playground
+//
+//  Created by Andrew on 29.01.2025.
+//
+
+#include "karatsuba.hpp"
---
+++ Playground/Karatsuba/karatsuba.hpp
@@ -0,0 +1,13 @@
+//
+//  karatsuba.hpp
+//  Playground
+//
+//  Created by Andrew on 29.01.2025.
+//
+
+#ifndef karatsuba_hpp
+#define karatsuba_hpp
+
+#include <stdio.h>
+
+#endif /* karatsuba_hpp */
